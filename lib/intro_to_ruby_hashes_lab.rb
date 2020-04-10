299517c517e952f26cb0d7eb75d4caa95ce18438
Jesse Langley <jlangleyiii@aol.com>
2020-04-10 03:43:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -4,15 +4,25 @@ def new_hash
 end
 
 def my_hash
-  
+  my_hash = {
+  :John => "Pastor",
+  :James => "Teacher"
+  }
+  my_hash
   # return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
+ pioneer = {
+  :name => 'Grass Hopper' 
+ }
+ pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
-
 def id_hash_generator(number)
+  id_hash_generator = {
+    :Bank => number
+  }
   # return a hash with a key :id assigned to the provided number
 end
